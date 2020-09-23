8a90fb83b4ac7e8287f59b5e11b46bd27d9e9579
Oleg Tielushko <tielushko@mail.usf.edu>
2020-09-22 22:34:38 -0400

    Start the first Python Game.

---
+++ pong.py
@@ -0,0 +1,23 @@
+import turtle
+
+game_window = turtle.Screen()
+game_window.title("Pong by Oleg Tielushko")
+game_window.bgcolor("black")
+game_window.setup(height=600, width=800)
+game_window.tracer(0)
+
+#Main game loop
+
+while True:
+    game_window.update()
+
+    #Paddle A
+
+    
+
+    #Paddle B
+
+
+    #Ball
+
+    
