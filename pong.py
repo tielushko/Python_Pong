935d34b8f8c494525f644cbcd312acf9f492466f
Oleg Tielushko <tielushko@mail.usf.edu>
2020-09-23 22:15:36 -0400

    Add paddles and a box. Start move functions

--- pong.py
+++ pong.py
@@ -8,16 +8,49 @@ game_window.tracer(0)
 
 #Main game loop
 
-while True:
-    game_window.update()
-
     #Paddle A
-
-    
+paddle_A = turtle.Turtle()
+paddle_A.speed(0)
+paddle_A.shape("square")
+paddle_A.color("white")
+paddle_A.shapesize(stretch_wid=5,stretch_len=1)
+paddle_A.penup()
+paddle_A.goto(-350,0)
 
     #Paddle B
-
+paddle_B = turtle.Turtle()
+paddle_B.speed(0)
+paddle_B.shape("square")
+paddle_B.color("white")
+paddle_B.shapesize(stretch_wid=5,stretch_len=1)
+paddle_B.penup()
+paddle_B.goto(350,0)
 
     #Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
 
-    
+    #Game functions
+
+#Move Paddle A -Up
+def paddle_A_up():
+    y = paddle_A.ycor()
+    y += 20
+    paddle_A.sety(y)
+
+#Move Paddle A -Down
+
+#Move Paddle B -Up
+
+#Move Paddle B -Down
+
+#Keyboard binding
+game_window.listen()
+game_window.onkeypress(paddle_A_up, "W")
+
+while True:
+    game_window.update()
