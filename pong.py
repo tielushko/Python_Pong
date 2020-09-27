6ddb5e49d0ff9c34c3e0e78506241c60666c0845
Oleg Tielushko <tielushko@mail.usf.edu>
2020-09-27 11:19:59 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -33,8 +33,11 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0,0)
+ball.dx = 0.15 #ball speed over x axis - delta x
+ball.dy = 0.15
 
-    #Game functions
+
+#Game functions
 
 #Move Paddle A -Up
 def paddle_A_up():
@@ -43,14 +46,64 @@ def paddle_A_up():
     paddle_A.sety(y)
 
 #Move Paddle A -Down
+def paddle_A_down():
+    y = paddle_A.ycor()
+    y -= 20
+    paddle_A.sety(y)
 
 #Move Paddle B -Up
+def paddle_B_up():
+    y = paddle_B.ycor()
+    y += 20
+    paddle_B.sety(y)
 
 #Move Paddle B -Down
+def paddle_B_down():
+    y = paddle_B.ycor()
+    y -= 20
+    paddle_B.sety(y)
 
 #Keyboard binding
 game_window.listen()
-game_window.onkeypress(paddle_A_up, "W")
+game_window.onkeypress(paddle_A_up, "w")
+game_window.onkeypress(paddle_A_down, "s")
+game_window.onkeypress(paddle_B_up, "Up")
+game_window.onkeypress(paddle_B_down, "Down")
 
 while True:
     game_window.update()
+
+    #Move the Ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #border checking conditions
+    
+    #top of the screen
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    #bottom of the screen
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    #right of the screen - out of bounds, reset to 0,0 and reverse direction
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+
+    #left of the screen - out of bounds, reset to 0,0 and reverse direction
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+
+    #paddle and ball collisions - right paddle
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_B.ycor() + 40 and ball.ycor() > paddle_B.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_A.ycor() + 40 and ball.ycor() > paddle_A.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
